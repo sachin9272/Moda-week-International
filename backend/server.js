89d29f7826c367d3ad13011db3331213b5f29a1d
Sachin Singh <sachinsingh9272@gmail.com>
2025-11-08 00:09:15 +0530

    Implemented Admin Route

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-
+import adminRoutes from './routes/adminRoutes.js';
 
 dotenv.config();
 // Connect to database
@@ -12,6 +12,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get('/', (req, res) => {
@@ -22,6 +23,9 @@ app.get('/', (req, res) => {
 app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
+
+// Custom Routes
+app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
