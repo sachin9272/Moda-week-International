8c7a145eaf3fa47d5fdb2ae6af04f8a71d53e0bd
Sachin Singh <sachinsingh9272@gmail.com>
2025-11-07 23:55:38 +0530

    Vite setup

--- adminPanel/vite.config.js
+++ adminPanel/vite.config.js
@@ -1,8 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
